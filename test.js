18977d4286a466a3f61d75a602db1c5f302a8bfc
mariomui <yi.kan.mui@gmail.com>
2018-10-04 17:53:04 -0500

    Running Un-updated Tweet Done
    Refresh for new tweets finished.
    Need to run tests to see if the functionality is working properly.
    No missing tweets.

--- test.js
+++ test.js
@@ -1,13 +1,6 @@
-window.taboo = {
-    test: 5,
-};
-var obj = (function () {
-    return {
-        batman: 5,
-    };
-})();
-
-
+setTimeout( () => { console.log('hi') }, 8000);
+console.log('bye');
+//function implementations are ignored until invoked.
 // const generateNons = () => {
 //     console.info('hi');
 // }
@@ -19,3 +12,25 @@ var obj = (function () {
 //   };
 // scheduleNextBarf();
 
+/*
+var generateRandomTweet = function(){
+  var tweet = {};
+  tweet.user = randomElement(users);
+  tweet.message = randomMessage();
+  tweet.created_at = new Date();
+  addTweet(tweet);
+
+create an object. then add this object that is the tweet-container
+
+var addTweet = function(newTweet){
+  var username = newTweet.user;
+  streams.users[username].push(newTweet);
+  streams.home.push(newTweet);
+};
+
+newTweet is the tweet container, and it creates the timeline in users.
+creates the stream of data in stream.home.
+
+};
+
+*/
